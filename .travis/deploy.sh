040bbe383f1066375a57badbe6ea8f7efed14914
conorRed <conorredington@gmail.com>
2019-08-02 16:36:19 +0100

    minor edit to deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,18 +1,21 @@
-et -xe
+set -xe
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
+  echo "Create ssh agent with key"
   eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_rsa
+  ssh-add ~/.ssh/travis_rsa
 
-  cd public
-  git init
-
-  git remote add deploy "travis@lir.com:/var/www/lir.com"
+  echo "Set up remote"
+  git remote add deploy "travis@46.101.23.183:/var/www/lir.com"
   git config user.name "Travis CI"
   git config user.email "travis@lir.com"
 
+  echo "Set up new ignore file"
+  rm -f .gitignore
+  cp .travis/deployignore .gitignore
   git add .
   git status # debug
+  echo "Commit"
   git commit -m "Deploy"
   git push -f deploy HEAD:master
 else
