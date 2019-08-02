257ee65e1dc471bb0d26d8ddd53229ed609e1469
Travis CI <travis@lir.com>
2019-08-02 19:59:25 +0100

    matching online deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,24 @@
 set -xe
 
-if [ $TRAVIS_BRANCH == 'master' ] ; then
+if [ $TRAVIS_BRANCH == "master" ] ; then
+  echo "Create ssh agent with key"
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/travis_rsa
+
+  echo "Set up remote"
+  git remote add deploy "travis@46.101.23.183:/var/www/lir.com"
+  git config user.name "Travis CI"
+  git config user.email "travis@lir.com"
+
+  echo "Set up new ignore file"
+  rm -f .gitignore
+  cp .travis/deployignore .gitignore
+  git add .
+  git status # debug
+  echo "Commit"
+  git commit -m "Deploy"
+  git push -f deploy HEAD:master
+elif [ $TRAVIS_BRANCH == "staging" ] ; then
   echo "Create ssh agent with key"
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/travis_rsa
